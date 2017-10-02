aacfb877569fed9fef6354bb553e0726b4b7251d
FMediotte96 <facumediotte@gmail.com>
2017-10-02 10:52:49 -0300

    Capa

--- TP3/src/infanteria/Capa.java
+++ TP3/src/infanteria/Capa.java
@@ -6,22 +6,9 @@ public class Capa extends ItemDecorator{
 		
 		super(unidad);
 		if(unidad instanceof Soldado) {
-			this.unidad = (Soldado)unidad;
-			if(((Soldado)unidad).getEnergia()<50)
-				((Soldado)unidad).setEnergia(((Soldado)unidad).getEnergia()*2);
-			else
-				((Soldado)unidad).setEnergia(100);
+			((Soldado)unidad).setEnergia(((Soldado)unidad).getEnergia()*2);
 		}	 
-	}
-	
-//	public Capa(Soldado soldado) {
-//		super(soldado);
-//		if(((Soldado)unidad).getEnergia()<50)
-//			((Soldado)unidad).setEnergia(soldado.getEnergia()*2);
-//		else
-//			((Soldado)unidad).setEnergia(100);
-//	}
-	
+	}	
 	
 	//ATAQUE
 	@Override
@@ -31,20 +18,3 @@ public class Capa extends ItemDecorator{
 		return unidad.getPuntosDaño();
 	}
 }
-
-
-//public Capa(Unidad unidad) {
-//	
-//		
-//		if(unidad instanceof Soldado) {
-//			super(soldado);
-//			this.unidad = (Soldado)unidad;
-//			if(((Soldado)unidad).getEnergia()<50)
-//				((Soldado)unidad).setEnergia(((Soldado)unidad).getEnergia()*2);
-//			else
-//				((Soldado)unidad).setEnergia(100);
-//		}
-//		else
-//			unidad = null;
-	 
-//}
